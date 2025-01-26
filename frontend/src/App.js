1fbcf716f45fde109bbd3244c64ae1fc2754958e
banksco <cdbankxx@gmail.com>
2025-01-25 18:55:32 -0600

    Added components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,12 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 const App = () => {
   return (
   <>
+    <Header />
     <h1>Welcome to my shop</h1>
+    <Footer />
   </>
   );
 }
