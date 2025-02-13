efe8fbb73385b14bd58115703c13eaa4cb31b5ed
banksco <cdbankxx@gmail.com>
2025-02-13 16:05:01 -0600

    Models and Data

---
+++ backend/seeder.js
@@ -0,0 +1,5 @@
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import Product from './models/productModel.js'
+import User from './models/userModel.js'
+import products from './data/products.js'
