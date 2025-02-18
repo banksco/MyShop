d55e543bbd8ff3d02a5ded8902fdad3525a7cf43
banksco <cdbankxx@gmail.com>
2025-02-18 10:05:52 -0600

    new type error

--- backend/seeder.js
+++ backend/seeder.js
@@ -15,6 +15,15 @@ const importData = async () => {
 
        const createdUsers = User.insertMany(users)
        const adminUser = createdUsers[0]._id
+
+       const sampleProducts = products.map(product => {
+        return {...product, user: adminUser}
+       })
+
+       await Product.insertMany(sampleProducts)
+
+       console.log('Data imported!')
+       process.exit(1)
     
     } catch (error){
         console.error(`Error: ${error.message}`)
@@ -23,3 +32,21 @@ const importData = async () => {
 
 }
 
+const destroyData = async () => {
+    try{
+        await Product.deleteMany()
+        await User.deleteMany()
+        console.log('Data destroyed!')
+        process.exit(1)
+
+    }catch (error){
+        console.error(`Error: ${error.message}`)
+        process.exit(1) 
+    }
+}
+
+if (process.argv[2] === '-d'){
+    destroyData()
+} else {
+importData()
+}
