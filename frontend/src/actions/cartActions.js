39d60224cb8e59d063d4adc12d4535ef4d623951
banksco <cdbankxx@gmail.com>
2025-03-10 15:32:26 -0500

    api fix

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -2,7 +2,7 @@ import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants"
 import axios from "axios"
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
-    const {data} = await axios.get(`api/products/${id}`)
+    const {data} = await axios.get(`/api/products/${id}`)
     dispatch({
         type: CART_ADD_ITEM,
         payload: {
