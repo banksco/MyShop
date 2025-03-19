4383c01a4704d51a2685e8a33ef7f1d5aaaf96b3
banksco <cdbankxx@gmail.com>
2025-03-19 14:34:11 -0500

    Shipping Screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -15,8 +15,15 @@ const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(loca
 
 const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
 
+const shippingAddressFromStorage = localStorage.getItem('shippingAddress') ? JSON.parse(localStorage.getItem('shippingAddress')) : {}
+
+const paymentMethodFromStorage = localStorage.getItem('paymentMethod') ? JSON.parse(localStorage.getItem('paymentMethod')) : {}
 const initialState = {
-    cart: {cartItems: cartItemsFromStorage},
+    cart: {
+        cartItems: cartItemsFromStorage,
+        shippingAdress: shippingAddressFromStorage, 
+        paymentMethod: paymentMethodFromStorage
+    },
     userLogin: {userInfo: userInfoFromStorage}
 }
 
